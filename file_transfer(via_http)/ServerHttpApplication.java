57f398997c25cb5cf38c77be48bd593985a95b9d
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-02-01 02:21:30 +0600

    Add files via upload

---
+++ file_transfer(via_http)/ServerHttpApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerHttpApplication.class, args);
+    }
+
+}
